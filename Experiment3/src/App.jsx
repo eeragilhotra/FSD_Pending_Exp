2147cf53309c25faac0181118f0fc00b6d5cc030
eeragilhotra <eeragilhotra05@gmail.com>
2026-04-24 17:25:08 +0530

    Add files via upload

---
+++ Experiment3/src/App.jsx
@@ -0,0 +1,12 @@
+import SPA from "./components/spa";
+
+function App() {
+  return (
+    <div style={{ padding: "20px", color: "white" }}>
+      <h1>My First SPA</h1>
+      <SPA />
+    </div>
+  );
+}
+
+export default App;
---
+++ Experiment3/src/components/About.jsx
@@ -0,0 +1,3 @@
+export default function About() {
+  return <h2>About Page exp3</h2>;
+}
